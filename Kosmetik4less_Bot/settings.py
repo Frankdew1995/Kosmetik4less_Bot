b88264b7acd1b59e555c4349dd264e1bb9f53c75
Frank H.J. Du <Frankdew1995@users.noreply.github.com>
2017-12-30 11:01:16 +0100

    Update settings.py

--- Kosmetik4less_Bot/settings.py
+++ Kosmetik4less_Bot/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'Kosmetik4less_Bot.spiders'
 #USER_AGENT = 'Kosmetik4less_Bot (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
