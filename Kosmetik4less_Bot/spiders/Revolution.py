19ad5cef9f78b937b0f0c53de02685600f9bb65f
Frank H.J. Du <Frankdew1995@users.noreply.github.com>
2017-12-30 11:09:20 +0100

    Update Revolution.py

--- Kosmetik4less_Bot/spiders/Revolution.py
+++ Kosmetik4less_Bot/spiders/Revolution.py
@@ -14,9 +14,11 @@ class CatriceSpider(scrapy.Spider):
         imgs = response.css("a.product-card__container img ::attr(data-src)").extract()
         for img in imgs:
             item['img'] = img
+            yield item
         for name in names: 
             item['name'] = name
-        yield item
+            yield item
+#         yield item
         base_url = "https://www.kosmetik4less.de/en/makeup-revolution?page={}"
 #         next_page_url = response.css("li.page-item > a.page-link::attr(href)")[4].extract()
         for i in range(2,16):
