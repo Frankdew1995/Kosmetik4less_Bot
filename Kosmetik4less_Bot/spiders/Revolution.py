b42f12082c0109f8968f5989baa4dd265f758c92
Frank H.J. Du <Frankdew1995@users.noreply.github.com>
2018-01-30 09:08:05 +0100

    Update Revolution.py

--- Kosmetik4less_Bot/spiders/Revolution.py
+++ Kosmetik4less_Bot/spiders/Revolution.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from Kosmetik4less_Bot.items import Kosmetik4LessBotItem
+# from Kosmetik4less_Bot.items import Kosmetik4LessBotItem
 
 
 class CatriceSpider(scrapy.Spider):
@@ -11,16 +11,17 @@ class CatriceSpider(scrapy.Spider):
     def parse(self, response):
 
         item = Kosmetik4LessBotItem()
-        for product in response.css("div.product-card__image.desktop"):
+        for product, price in response.css("div.product-card__image.desktop") and response.css("div.product-card__prices"):
 
             item['img'] = product.css("img.lazyload::attr(data-src)").extract_first()
             item['name'] = product.css("img.lazyload::attr(alt)")[0].extract()
-            yield item
-            
-        for price in response.css("div.product-card__prices"):
-            
             item['price'] = price.css("strong.product-card__price::text").extract_first()
             yield item
+            
+#         for price in response.css("div.product-card__prices"):
+            
+#             item['price'] = price.css("strong.product-card__price::text").extract_first()
+#             yield item
 
         base_url = "https://www.kosmetik4less.de/en/makeup-revolution?page={}"
 
